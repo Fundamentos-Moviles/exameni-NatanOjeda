a146f218aadb4e37e737aa62dbd31e9d5cd20584
NatanOjeda <127531018+NatanOjeda@users.noreply.github.com>
2025-09-19 00:04:42 -0600

    Add lib and Dart Files
    Añadí la carpeta lib, el archivo file, y archivo memorama, que serán la base con lo que trabajare en el desarrollo, más adelante, conforme lo vaya necesitando, posiblemente agregare más archivos

---
+++ lib/memorama.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Memorama extends StatefulWidget {
+  const Memorama({super.key});
+
+  @override
+  State<Memorama> createState() => _MemoramaState();
+}
+
+class _MemoramaState extends State<Memorama> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
