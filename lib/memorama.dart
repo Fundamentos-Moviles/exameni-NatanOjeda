614d391b606e73ddc2eb342c4b11aba03f66c9b8
NatanOjeda <127531018+NatanOjeda@users.noreply.github.com>
2025-09-19 22:56:00 -0600

    Add initState
    Agregue la función para iniciar el juego, se calcula el número total de cuadros aquí, y se verifica que sea par, y se llama la función para inicializar el juego

--- lib/memorama.dart
+++ lib/memorama.dart
@@ -20,11 +20,17 @@ class _MemoramaState extends State<Memorama> {
   final Color gris = Colors.grey;
   bool esperando = false;
 
-  void inicializaJuego(){
+  @override
+  void initState(){
+    super.initState();
     totalCuadros = filas * columnas;
     if(totalCuadros%2==0){
       throw Exception("El número de cuadros debe ser par");
     }
+    inicializaJuego();
+  }
+
+  void inicializaJuego(){
     final Random random = Random();
     List<Color> baseColores = List.generate(totalCuadros ~/ 2, (index) {
       return Color.fromARGB(
@@ -48,7 +54,6 @@ class _MemoramaState extends State<Memorama> {
     setState(() {});
   }
 
-  @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
