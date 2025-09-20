32674c7cff564f741e85310901728e221568a30a
NatanOjeda <127531018+NatanOjeda@users.noreply.github.com>
2025-09-19 22:18:12 -0600

    Creación variables para programa
    Cree las variables necesarias para usar en el programa, que aunque se expliquen por si solas la mayoría según su función, la variable esperando es para evitar que más de un cuadro sea seleccionado al momento de seleccionar un cuadro

--- lib/memorama.dart
+++ lib/memorama.dart
@@ -8,6 +8,16 @@ class Memorama extends StatefulWidget {
 }
 
 class _MemoramaState extends State<Memorama> {
+  int filas = 4;
+  int columnas = 5;
+  late int totalCuadros;
+  late List<Color> colores;
+  late List<bool> visibles;
+  int? primerIndice;
+  int? segundoIndice;
+  final Color gris = Colors.grey;
+  bool esperando = false;
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
