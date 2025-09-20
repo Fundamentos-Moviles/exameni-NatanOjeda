350eaafbeb68f40261ca728aae44d6ae02587772
NatanOjeda <127531018+NatanOjeda@users.noreply.github.com>
2025-09-19 22:48:23 -0600

    Add InicializaJuego
    En esta función se genera una lista de colores random (según la mitad del número total de cuadros), luego se crea la lista de colores (2 por cada uno), y con el shufle los mezclo aleatoriamente, además inicializo valores valores del juego, creando la lista de cuadros visibles, todos con la bandera de falso.

--- lib/memorama.dart
+++ lib/memorama.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Memorama extends StatefulWidget {
@@ -17,6 +19,34 @@ class _MemoramaState extends State<Memorama> {
   int? segundoIndice;
   final Color gris = Colors.grey;
   bool esperando = false;
+
+  void inicializaJuego(){
+    totalCuadros = filas * columnas;
+    if(totalCuadros%2==0){
+      throw Exception("El número de cuadros debe ser par");
+    }
+    final Random random = Random();
+    List<Color> baseColores = List.generate(totalCuadros ~/ 2, (index) {
+      return Color.fromARGB(
+        255,
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+      );
+    });
+
+    colores = List.from(baseColores);
+    colores.addAll(baseColores);
+    colores.shuffle();
+
+    visibles = List.generate(totalCuadros, (index) => false);
+
+    primerIndice = null;
+    segundoIndice = null;
+    esperando = false;
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
